08fe1f9f9389ef968f6cabd5419c7e4c140152b4
HuongNTN <huongntn@mecorp.vn>
2019-09-21 10:24:14 +0700

    bt

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ app.get('/:name', (req, res) => {
     let name = req.params.name
     res.send({ name });
 })
+/**
+ * cho đường dẫn: http://localhost:3000/cong/3/4
+ * Trong đó:
+ *           cong: ten phep tinh
+ *           3: số a
+ *           4: số b
+ * Yêu cầu:
+ * - Thực hiên phép tính: a + b và in kết quả ra ngoài màn hình
+ * - Nếu thay đổi ten phep tinh cong -> tru : Thực hiên phép tính a-b
+ * - Nếu thay đổi ten phep tinh nhan -> nhan : Thực hiên phép tính a*b
+ * - Nếu thay đổi ten phep tinh chia -> chia : Thực hiên phép tính a/b
+ */
