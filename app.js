8a3109503fe8802dcaecd3f9d2830c62bbbd433b
HuongNTN <huongntn@mecorp.vn>
2019-09-21 09:42:09 +0700

    get

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // npm init --y
 const express = require('express');
 const app = express();
-
 app.listen(3000, () => console.log('Server is running on port 3000'));
+
+app.get('/', (req, res) => {
+    res.send({
+        name: 'Teo'
+    })
+})
