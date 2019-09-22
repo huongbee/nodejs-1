5f06645669bb166f7d86c3b67d96f18d221a7958
HuongNTN <huongntn@mecorp.vn>
2019-09-22 10:04:41 +0700

    login

--- middleware.js
+++ middleware.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Server is running on port 3000'));
 
+function kiemtraDangnhap(req, res, next) {
+    const { username, password } = req.params;
+    if (username !== 'admin') {
+        res.send('Invalid username!');
+        return;
+    }
+    if (password !== '123456') {
+        res.send('Invalid password!');
+        return;
+    }
+    next()
+}
+app.get('/login/:username/:password', kiemtraDangnhap, (req, res) => {
+    res.send(`Hello ${req.params.username}`);
+})
+
 /**
  * cho đường dẫn: http://localhost:3000/login/:username/:password
  *
